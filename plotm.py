ee2f9fc4a08839e1ce2d1464df3a0173e79114a7
Caleb Dennis <caleb.dennis429@gmail.com>
2022-07-25 15:45:57 -0700

    Added measurement density plot to analysis script

--- plotm.py
+++ plotm.py
@@ -5,6 +5,10 @@ from statistics import median, stdev
 import os
 import sys
 
+
+seconds_in_year = 3600 * 24 * 365
+
+
 def main(data_path):
     data = []
     for data_file in filter(lambda de: de.is_file(), os.scandir(data_path)):
@@ -15,8 +19,9 @@ def main(data_path):
 
     print(f"found {len(data)} well-named files")
 
-    outlier_file_names(data)
-    file_names_minute_allignment(data, allign=6)
+    # outlier_file_names(data)
+    # file_names_minute_allignment(data, allign=6)
+    measurement_density(data)
     plt.show(block=True)
 
 
@@ -25,7 +30,7 @@ def outlier_file_names(file_name_data):
     dev = stdev(file_name_data)
     outliers = list(
             filter(
-                lambda num: abs(num - med) > 3*dev,
+                lambda num: abs(num - med) > 4*dev,
                 file_name_data
             )
         )
@@ -41,6 +46,29 @@ def file_names_minute_allignment(file_name_data, allign=2, name="File Names % 60
         if b%allign == 0 or (b+1)%allign == 0:
             p.set_facecolor(mcolors.CSS4_COLORS["darkviolet"])
 
+
+def measurement_density(file_name_data):
+    med = median(file_name_data)
+    # do outlier rejection based on premise that test lasted less than a year
+    corrected = list(filter(lambda num: abs(num - med) < seconds_in_year, file_name_data))
+    corrected.sort()
+    density = [[],[0]]
+    hour_zero = corrected[0] // 3600
+    current_hr = hour_zero
+    for time in corrected:
+        if time // 3600 > current_hr:
+            density[0].append(current_hr - hour_zero)
+            density[1].append(0)
+            current_hr = time // 3600
+        density[1][-1] += 1
+
+    density[0].append(current_hr - hour_zero)
+
+    fig, ax = plt.subplots()
+    ax.set_title("Measurement density")
+    ax.set_xlabel("Hours since measurement began")
+    ax.set_ylabel("# Measurements")
+    ax.scatter(density[0], density[1])
 
 
 
